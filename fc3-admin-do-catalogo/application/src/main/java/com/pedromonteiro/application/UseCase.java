9edd7feffeb2805bfd19f483bb6ca42961ce3493
Pedro Monteiro <pedromont436@gmail.com>
2023-12-02 00:40:24 -0300

    criando entity Category

--- fc3-admin-do-catalogo/application/src/main/java/com/pedromonteiro/application/UseCase.java
+++ fc3-admin-do-catalogo/application/src/main/java/com/pedromonteiro/application/UseCase.java
@@ -1,6 +1,6 @@
 package com.pedromonteiro.application;
 
-import com.pedromonteiro.domain.Category;
+import com.pedromonteiro.domain.category.Category;
 
 public class UseCase {
     public Category execute() {
--- fc3-admin-do-catalogo/domain/src/main/java/com/pedromonteiro/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.pedromonteiro.domain;
-
-public class Category {
-    public String id;
-}
