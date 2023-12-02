60cbfe573b7f9c23a26692da8e75f6b35433a842
Pedro Monteiro <pedromont436@gmail.com>
2023-12-01 21:17:21 -0300

    primeiro commit

---
+++ fc3-admin-do-catalogo/src/main/java/br/com/pedromonteiro/fc3admindocatalogo/Fc3AdminDoCatalogoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedromonteiro.fc3admindocatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fc3AdminDoCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fc3AdminDoCatalogoApplication.class, args);
+	}
+
+}
---
+++ fc3-admin-do-catalogo/src/test/java/br/com/pedromonteiro/fc3admindocatalogo/Fc3AdminDoCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedromonteiro.fc3admindocatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fc3AdminDoCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
