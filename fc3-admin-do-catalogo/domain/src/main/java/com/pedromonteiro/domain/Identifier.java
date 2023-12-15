9f882bb2494d8da3fcce0fa0a3553b4a6974a7c6
Pedro Monteiro <pedromont436@gmail.com>
2023-12-14 22:22:22 -0300

    criando testes de validacao

--- fc3-admin-do-catalogo/domain/src/main/java/com/pedromonteiro/domain/Identifier.java
+++ fc3-admin-do-catalogo/domain/src/main/java/com/pedromonteiro/domain/Identifier.java
@@ -1,5 +1,6 @@
 package com.pedromonteiro.domain;
 
 public abstract class Identifier extends ValueObject {
-    
+
+    public abstract String getValue();
 }
--- fc3-admin-do-catalogo/domain/src/test/java/com/pedromonteiro/domain/category/CategoryTest.java
+++ fc3-admin-do-catalogo/domain/src/test/java/com/pedromonteiro/domain/category/CategoryTest.java
@@ -6,25 +6,23 @@ import org.junit.jupiter.api.Test;
 public class CategoryTest {
     
     @Test
-    void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+    void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
 
-        final var expectedName = "Filmes";
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
         final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
 
         final var actualCategory = 
                 Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
-        Assertions.assertNotNull(actualCategory);
-        Assertions.assertNotNull(actualCategory.getId());
-        Assertions.assertEquals(expectedName, actualCategory.getName());
-        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
-        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
-        Assertions.assertNotNull(actualCategory.getCreatedAt());
-        Assertions.assertNotNull(actualCategory.getUpdatedAt());
-        Assertions.assertNull(actualCategory.getDeletedAt());
-    
 
+       final var actualException = 
+              Assertions.assertThrows(DomainException.class, actualCategory.validate());
+
+              Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+              Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).getMessage());
         
     }
 }
