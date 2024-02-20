499a59ecd76c01c8a21f18c750e50c41d1d15971
Pedro Monteiro <pedromont436@gmail.com>
2024-02-20 00:58:00 -0300

    configurando os profiles de springboot

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/Main.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/Main.java
@@ -3,6 +3,7 @@ package com.pedromonteiro.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 import com.pedromonteiro.infrastructure.configuration.WebServerConfig;
 
@@ -10,5 +11,6 @@ import com.pedromonteiro.infrastructure.configuration.WebServerConfig;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(WebServerConfig.class,args);
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
     }
 }
