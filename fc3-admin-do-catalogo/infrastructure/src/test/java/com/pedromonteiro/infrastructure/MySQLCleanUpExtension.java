81187333b04398232eb192a8b4a50162582e0ce2
Pedro Monteiro <pedromont436@gmail.com>
2024-02-23 00:13:32 -0300

    começando com testes de CategoryMySQLGateway

--- fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/infrastructure/MySQLCleanUpExtension.java
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/infrastructure/MySQLCleanUpExtension.java
@@ -1,24 +1,19 @@
 package com.pedromonteiro.infrastructure;
 
+import java.util.Collection;
+
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.pedromonteiro.infrastructure.category.persistence.CategoryRepository;
-
-import java.util.Collection;
-import java.util.List;
-
 public class MySQLCleanUpExtension implements BeforeEachCallback {
 
     @Override
     public void beforeEach(final ExtensionContext context) {
-        final var appContext = SpringExtension.getApplicationContext(context);
-
-        cleanUp(List.of(
-                appContext.getBean(CategoryRepository.class)
-        ));
+        final var appContext = SpringExtension.getApplicationContext(context)
+         .getBeansOfType(CrudRepository.class)
+         .values();
     }
 
     private void cleanUp(final Collection<CrudRepository> repositories) {
--- fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/infrastructure/category/CategoryMySQLGatewayTest.java
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,10 +1,13 @@
 package com.pedromonteiro.infrastructure.category;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.pedromonteiro.domain.category.Category;
 import com.pedromonteiro.infrastructure.MySQLGatewayTest;
 import com.pedromonteiro.infrastructure.category.persistence.CategoryRepository;
 
@@ -19,10 +22,38 @@ public class CategoryMySQLGatewayTest {
     private CategoryRepository categoryRepository;
 
     @Test
-    public void testInjectedDependencies() {
-        assertNotNull(categoryGateway);
-        assertNotNull(categoryRepository);
-   
+    public void givenAValidCategory_whenCallsCreate_shouldReturnANewCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        assertEquals(0, categoryRepository.count());
+
+        final var actualCategory = categoryGateway.create(aCategory);
+
+        assertEquals(1, categoryRepository.count());
+
+        assertEquals(aCategory.getId(), actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+        assertEquals(aCategory.getUpdatedAt(), actualCategory.getUpdatedAt());
+        assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
+        assertNull(actualCategory.getDeletedAt());
+
+        final var actualEntity = categoryRepository.findById(aCategory.getId().getValue()).get();
+
+        assertEquals(aCategory.getId().getValue(), actualEntity.getId());
+        assertEquals(expectedName, actualEntity.getName());
+        assertEquals(expectedDescription, actualEntity.getDescription());
+        assertEquals(expectedIsActive, actualEntity.isActive());
+        assertEquals(aCategory.getCreatedAt(), actualEntity.getCreatedAt());
+        assertEquals(aCategory.getUpdatedAt(), actualEntity.getUpdatedAt());
+        assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
+        assertNull(actualEntity.getDeletedAt());
     }
 
     
