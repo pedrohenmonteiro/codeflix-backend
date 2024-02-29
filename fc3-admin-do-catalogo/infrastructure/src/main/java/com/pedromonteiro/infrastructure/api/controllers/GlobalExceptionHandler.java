20ac3446e44d6003ffa3ed592631a91f6f093023
Pedro Monteiro <pedromont436@gmail.com>
2024-02-29 01:43:30 -0300

    criando um handler de erro global

---
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.pedromonteiro.infrastructure.api.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.pedromonteiro.domain.exceptions.DomainException;
+import com.pedromonteiro.domain.validation.Error;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = DomainException.class)
+    public ResponseEntity<?> handleDomainException(final DomainException ex) {
+        return ResponseEntity.unprocessableEntity().body(ApiError.from(ex));
+    }
+
+    record ApiError(String message, List<Error> errors) {
+        static ApiError from(final DomainException ex) {
+            return new ApiError(ex.getMessage(), ex.getErrors());
+        }
+    }
+}
--- fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/api/CategoryAPITest.java
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/api/CategoryAPITest.java
@@ -1,7 +1,10 @@
 package com.pedromonteiro.api;
 
+import static io.vavr.API.Left;
 import static io.vavr.API.Right;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.times;
@@ -30,10 +33,13 @@ import com.pedromonteiro.application.category.delete.DeleteCategoryUseCase;
 import com.pedromonteiro.application.category.retrieve.get.GetCategoryByIdUseCase;
 import com.pedromonteiro.application.category.retrieve.list.ListCategoriesUseCase;
 import com.pedromonteiro.application.category.update.UpdateCategoryUseCase;
+import com.pedromonteiro.domain.exceptions.DomainException;
+import com.pedromonteiro.domain.validation.handler.Notification;
 import com.pedromonteiro.infrastructure.api.CategoryAPI;
 import com.pedromonteiro.infrastructure.category.models.CreateCategoryRequest;
 
 @ControllerTest(controllers = CategoryAPI.class)
+@SpringBootTest(classes = CategoryAPI.class)
 public class CategoryAPITest {
 
     @Autowired
@@ -83,6 +89,81 @@ public class CategoryAPITest {
                 .andExpect(header().string("Location", "/categories/123"))
                 .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.id", equalTo("123")));
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+
+        @Test
+    public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+        // given
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedMessage = "'name' should not be null";
+
+        final var aInput =
+                new CreateCategoryRequest(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(Left(Notification.create(new Error(expectedMessage))));
+
+        // when
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location", nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+
+     @Test
+    public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+        // given
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedMessage = "'name' should not be null";
+
+        final var aInput =
+                new CreateCategoryRequest(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenThrow(DomainException.with(new Error(expectedMessage)));
+
+        // when
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location", nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.message", equalTo(expectedMessage)))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
 
         verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
                 Objects.equals(expectedName, cmd.name())
