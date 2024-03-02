0fc43eba110137e282279add5145a1653d7f5237
Pedro Monteiro <pedromont436@gmail.com>
2024-03-02 00:23:45 -0300

    testes e2e de criaçao de categoria

--- fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/e2e/category/CategoryE2E.java
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/e2e/category/CategoryE2E.java
@@ -1,5 +1,7 @@
 package com.pedromonteiro.e2e.category;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -9,13 +11,30 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.pedromonteiro.E2ETest;
+import com.pedromonteiro.domain.category.CategoryID;
+import com.pedromonteiro.infrastructure.category.models.CreateCategoryRequest;
 import com.pedromonteiro.infrastructure.category.persistence.CategoryRepository;
+import com.pedromonteiro.infrastructure.configuration.json.Json;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @E2ETest
 @Testcontainers
 public class CategoryE2E {
 
+    @Autowired
+    private MockMvc mvc;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
     
     @Container
     private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
@@ -28,4 +47,45 @@ public class CategoryE2E {
         registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
     }
 
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = categoryRepository.findById(actualId.getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+        final var aRequestBody = new CreateCategoryRequest(aName, aDescription, isActive);
+        final var actualId = this.given("/categories", aRequestBody);
+        return CategoryID.from(actualId);
+    }
+
+    private String given(final String url, final Object body) throws Exception {
+        final var aRequest = post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(body));
+                
+        final var actualId = this.mvc.perform(aRequest)
+        .andExpect(status().isCreated())
+        .andReturn()
+        .getResponse().getHeader("Location")
+        .replace("%s/".formatted(url), "");
+
+        return actualId;
+    }
 }
