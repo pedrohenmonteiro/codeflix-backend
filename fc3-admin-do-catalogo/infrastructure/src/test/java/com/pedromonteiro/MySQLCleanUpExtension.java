1e224020ddafde714dcb53e2673b606670d25349
Pedro Monteiro <pedromont436@gmail.com>
2024-03-14 01:10:06 -0300

    criando os testes de GenreMySQLGatewayTest

--- fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/MySQLCleanUpExtension.java
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/MySQLCleanUpExtension.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.pedromonteiro.infrastructure.category.persistence.CategoryRepository;
+import com.pedromonteiro.infrastructure.genre.persistence.GenreRepository;
 
 public class MySQLCleanUpExtension implements BeforeEachCallback {
 
@@ -17,8 +19,14 @@ public class MySQLCleanUpExtension implements BeforeEachCallback {
         final var appContext = SpringExtension.getApplicationContext(context);
 
         cleanUp(List.of(
+                appContext.getBean(GenreRepository.class),
                 appContext.getBean(CategoryRepository.class)
         ));
+
+        final var em = appContext.getBean(TestEntityManager.class);
+
+        em.flush();
+        em.clear();
     }
 
     private void cleanUp(final Collection<CrudRepository> repositories) {
--- fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/infrastructure/api/CategoryAPITest.java
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/infrastructure/api/CategoryAPITest.java
@@ -1,4 +1,4 @@
-package com.pedromonteiro.api;
+package com.pedromonteiro.infrastructure.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
