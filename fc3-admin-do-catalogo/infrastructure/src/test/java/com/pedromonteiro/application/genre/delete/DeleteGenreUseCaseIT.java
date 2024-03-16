c81ce39449a34f5e47e9c814d7205dc6bd278db7
Pedro Monteiro <pedromont436@gmail.com>
2024-03-15 22:53:34 -0300

    implementando testes integrados do caso de uso de delete

---
+++ fc3-admin-do-catalogo/infrastructure/src/test/java/com/pedromonteiro/application/genre/delete/DeleteGenreUseCaseIT.java
@@ -0,0 +1,56 @@
+package com.pedromonteiro.application.genre.delete;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pedromonteiro.IntegrationTest;
+import com.pedromonteiro.domain.genre.Genre;
+import com.pedromonteiro.domain.genre.GenreGateway;
+import com.pedromonteiro.domain.genre.GenreID;
+import com.pedromonteiro.infrastructure.genre.persistence.GenreRepository;
+
+@IntegrationTest
+public class DeleteGenreUseCaseIT {
+
+    @Autowired
+    private DeleteGenreUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenre_shouldDeleteGenre() {
+        // given
+        final var aGenre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = aGenre.getId();
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // when
+        Assertions.assertEquals(0, genreRepository.count());
+    }
+
+    @Test
+    public void givenAnInvalidGenreId_whenCallsDeleteGenre_shouldBeOk() {
+        // given
+        genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = GenreID.from("123");
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // when
+        Assertions.assertEquals(1, genreRepository.count());
+    }
+}
