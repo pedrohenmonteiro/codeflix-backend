81b8645a55fd92c1254f113ff8f5f952a623834e
Pedro Monteiro <pedromont436@gmail.com>
2024-03-16 15:33:45 -0300

    criando testes de serializacao e desserializacao de genre api

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/GenreController.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/GenreController.java
@@ -1,50 +1,95 @@
 package com.pedromonteiro.infrastructure.api.controllers;
 
-import org.springframework.http.ResponseEntity;
+import java.net.URI;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedromonteiro.application.genre.create.CreateGenreCommand;
+import com.pedromonteiro.application.genre.create.CreateGenreUseCase;
+import com.pedromonteiro.application.genre.delete.DeleteGenreUseCase;
+import com.pedromonteiro.application.genre.retrieve.get.GetGenreByIdUseCase;
+import com.pedromonteiro.application.genre.retrieve.list.ListGenreUseCase;
+import com.pedromonteiro.application.genre.update.UpdateGenreCommand;
+import com.pedromonteiro.application.genre.update.UpdateGenreUseCase;
 import com.pedromonteiro.domain.pagination.Pagination;
+import com.pedromonteiro.domain.pagination.SearchQuery;
 import com.pedromonteiro.infrastructure.api.GenreAPI;
 import com.pedromonteiro.infrastructure.genre.models.CreateGenreRequest;
 import com.pedromonteiro.infrastructure.genre.models.GenreListResponse;
 import com.pedromonteiro.infrastructure.genre.models.GenreResponse;
 import com.pedromonteiro.infrastructure.genre.models.UpdateGenreRequest;
+import com.pedromonteiro.infrastructure.genre.presenters.GenreApiPresenter;
 
+@RestController
 public class GenreController implements GenreAPI {
+
+    private final CreateGenreUseCase createGenreUseCase;
+    private final DeleteGenreUseCase deleteGenreUseCase;
+    private final GetGenreByIdUseCase getGenreByIdUseCase;
+    private final ListGenreUseCase listGenreUseCase;
+    private final UpdateGenreUseCase updateGenreUseCase;
+
+    public GenreController(
+            final CreateGenreUseCase createGenreUseCase,
+            final DeleteGenreUseCase deleteGenreUseCase,
+            final GetGenreByIdUseCase getGenreByIdUseCase,
+            final ListGenreUseCase listGenreUseCase,
+            final UpdateGenreUseCase updateGenreUseCase
+    ) {
+        this.createGenreUseCase = createGenreUseCase;
+        this.deleteGenreUseCase = deleteGenreUseCase;
+        this.getGenreByIdUseCase = getGenreByIdUseCase;
+        this.listGenreUseCase = listGenreUseCase;
+        this.updateGenreUseCase = updateGenreUseCase;
+    }
 
     @Override
     public ResponseEntity<?> create(final CreateGenreRequest input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        final var aCommand = CreateGenreCommand.with(
+                input.name(),
+                input.isActive(),
+                input.categories()
+        );
+
+        final var output = this.createGenreUseCase.execute(aCommand);
+
+        return ResponseEntity.created(URI.create("/genres/" + output.id())).body(output);
     }
 
     @Override
     public Pagination<GenreListResponse> list(
-        final String search,
-        final int page,
-        final int perPage,
-        final String sort,
-        final String direction
-        ) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'list'");
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction
+    ) {
+        return this.listGenreUseCase.execute(new SearchQuery(page, perPage, search, sort, direction))
+                .map(GenreApiPresenter::present);
     }
 
     @Override
     public GenreResponse getById(final String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+        return GenreApiPresenter.present(this.getGenreByIdUseCase.execute(id));
     }
 
     @Override
     public ResponseEntity<?> updateById(final String id, final UpdateGenreRequest input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateById'");
+        final var aCommand = UpdateGenreCommand.with(
+                id,
+                input.name(),
+                input.isActive(),
+                input.categories()
+        );
+
+        final var output = this.updateGenreUseCase.execute(aCommand);
+
+        return ResponseEntity.ok(output);
     }
 
     @Override
     public void deleteById(final String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        this.deleteGenreUseCase.execute(id);
     }
-    
 }
