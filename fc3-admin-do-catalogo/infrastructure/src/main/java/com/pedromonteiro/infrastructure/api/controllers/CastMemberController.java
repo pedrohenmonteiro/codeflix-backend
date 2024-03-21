9c57f196b5a27cd32df9913ebce9ac94ec8796ad
Pedro Monteiro <pedromont436@gmail.com>
2024-03-21 00:51:27 -0300

    implementando castmember controller

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/CastMemberController.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/CastMemberController.java
@@ -1,45 +1,89 @@
 package com.pedromonteiro.infrastructure.api.controllers;
 
-import org.springframework.http.ResponseEntity;
+import java.net.URI;
+import java.util.Objects;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedromonteiro.application.castmember.create.CreateCastMemberCommand;
+import com.pedromonteiro.application.castmember.create.CreateCastMemberUseCase;
+import com.pedromonteiro.application.castmember.delete.DeleteCastMemberUseCase;
+import com.pedromonteiro.application.castmember.retrieve.get.GetCastMemberByIdUseCase;
+import com.pedromonteiro.application.castmember.retrieve.list.ListCastMembersUseCase;
+import com.pedromonteiro.application.castmember.update.UpdateCastMemberCommand;
+import com.pedromonteiro.application.castmember.update.UpdateCastMemberUseCase;
 import com.pedromonteiro.domain.pagination.Pagination;
+import com.pedromonteiro.domain.pagination.SearchQuery;
 import com.pedromonteiro.infrastructure.api.CastMemberAPI;
 import com.pedromonteiro.infrastructure.castmember.models.CastMemberListResponse;
 import com.pedromonteiro.infrastructure.castmember.models.CastMemberResponse;
 import com.pedromonteiro.infrastructure.castmember.models.CreateCastMemberRequest;
 import com.pedromonteiro.infrastructure.castmember.models.UpdateCastMemberRequest;
+import com.pedromonteiro.infrastructure.castmember.presenter.CastMemberPresenter;
 
+@RestController
 public class CastMemberController implements CastMemberAPI {
 
-    @Override
-    public ResponseEntity<?> create(CreateCastMemberRequest input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    private final CreateCastMemberUseCase createCastMemberUseCase;
+    private final GetCastMemberByIdUseCase getCastMemberByIdUseCase;
+    private final UpdateCastMemberUseCase updateCastMemberUseCase;
+    private final DeleteCastMemberUseCase deleteCastMemberUseCase;
+    private final ListCastMembersUseCase listCastMembersUseCase;
+
+    public CastMemberController(
+            final CreateCastMemberUseCase createCastMemberUseCase,
+            final GetCastMemberByIdUseCase getCastMemberByIdUseCase,
+            final UpdateCastMemberUseCase updateCastMemberUseCase,
+            final DeleteCastMemberUseCase deleteCastMemberUseCase,
+            final ListCastMembersUseCase listCastMembersUseCase
+    ) {
+        this.createCastMemberUseCase = Objects.requireNonNull(createCastMemberUseCase);
+        this.getCastMemberByIdUseCase = Objects.requireNonNull(getCastMemberByIdUseCase);
+        this.updateCastMemberUseCase = Objects.requireNonNull(updateCastMemberUseCase);
+        this.deleteCastMemberUseCase = Objects.requireNonNull(deleteCastMemberUseCase);
+        this.listCastMembersUseCase = Objects.requireNonNull(listCastMembersUseCase);
     }
 
     @Override
-    public Pagination<CastMemberListResponse> list(String search, int page, int perPage, String sort,
-            String direction) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'list'");
+    public ResponseEntity<?> create(final CreateCastMemberRequest input) {
+        final var aCommand =
+                CreateCastMemberCommand.with(input.name(), input.type());
+
+        final var output = this.createCastMemberUseCase.execute(aCommand);
+
+        return ResponseEntity.created(URI.create("/cast_members/" + output.id())).body(output);
     }
 
     @Override
-    public CastMemberResponse getById(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    public Pagination<CastMemberListResponse> list(
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction
+    ) {
+        return this.listCastMembersUseCase.execute(new SearchQuery(page, perPage, search, sort, direction))
+                .map(CastMemberPresenter::present);
     }
 
     @Override
-    public ResponseEntity<?> updateById(String id, UpdateCastMemberRequest aBody) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateById'");
+    public CastMemberResponse getById(final String id) {
+        return CastMemberPresenter.present(this.getCastMemberByIdUseCase.execute(id));
     }
 
     @Override
-    public void deleteById(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
-    } 
-    
+    public ResponseEntity<?> updateById(final String id, final UpdateCastMemberRequest aBody) {
+        final var aCommand =
+                UpdateCastMemberCommand.with(id, aBody.name(), aBody.type());
+
+        final var output = this.updateCastMemberUseCase.execute(aCommand);
+
+        return ResponseEntity.ok(output);
+    }
+
+    @Override
+    public void deleteById(final String id) {
+        this.deleteCastMemberUseCase.execute(id);
+    }
 }
