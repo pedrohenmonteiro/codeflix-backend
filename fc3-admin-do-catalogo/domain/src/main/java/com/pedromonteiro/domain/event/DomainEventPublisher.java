c495d204a7374de5412ea6c5cbb671cb2d072d8e
Pedro Monteiro <pedromont436@gmail.com>
2024-03-30 15:03:58 -0300

    testes unitários de domain events na classe entity

--- fc3-admin-do-catalogo/domain/src/main/java/com/pedromonteiro/domain/event/DomainEventPublisher.java
+++ fc3-admin-do-catalogo/domain/src/main/java/com/pedromonteiro/domain/event/DomainEventPublisher.java
@@ -2,6 +2,6 @@ package com.pedromonteiro.domain.event;
 
 @FunctionalInterface
 public interface DomainEventPublisher {
-    <T extends DomainEvent> void publishEvent(T event);
+     void publishEvent(DomainEvent event);
     
 }
