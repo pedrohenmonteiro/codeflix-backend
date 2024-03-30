1a3eecf7b78f09529ff1d0b0090f84a6c4369e50
Pedro Monteiro <pedromont436@gmail.com>
2024-03-30 12:35:42 -0300

    qualificando beans para injecao spring

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/configuration/AmqpConfig.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/configuration/AmqpConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.pedromonteiro.infrastructure.configuration.annotations.VideoCreatedQueue;
+import com.pedromonteiro.infrastructure.configuration.annotations.VideoEncodedQueue;
 import com.pedromonteiro.infrastructure.configuration.properties.amqp.QueueProperties;
 
 @Configuration
@@ -11,15 +13,20 @@ public class AmqpConfig {
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-created")
+    @VideoCreatedQueue
     QueueProperties videoCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-encoded")
+    @VideoEncodedQueue
     QueueProperties videoEncodedQueueProperties() {
         return new QueueProperties();
     }
 
-
+    @Bean
+    public String queueName(@VideoCreatedQueue QueueProperties props) {
+        return props.getQueue();
+    }
 }
