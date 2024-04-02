dc8295e074a0ed76731e03c55e77272820f687d7
Pedro Monteiro <pedromont436@gmail.com>
2024-04-02 01:15:24 -0300

    criando testes e implementando metodo de criar video full

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/VideoController.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/pedromonteiro/infrastructure/api/controllers/VideoController.java
@@ -1,18 +1,57 @@
 package com.pedromonteiro.infrastructure.api.controllers;
 
+import java.net.URI;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.pedromonteiro.application.video.create.CreateVideoCommand;
+import com.pedromonteiro.application.video.create.CreateVideoUseCase;
+import com.pedromonteiro.application.video.delete.DeleteVideoUseCase;
+import com.pedromonteiro.application.video.media.get.GetMediaUseCase;
+import com.pedromonteiro.application.video.media.upload.UploadMediaUseCase;
+import com.pedromonteiro.application.video.retrieve.get.GetVideoByIdUseCase;
+import com.pedromonteiro.application.video.retrieve.list.ListVideosUseCase;
+import com.pedromonteiro.application.video.update.UpdateVideoUseCase;
 import com.pedromonteiro.domain.pagination.Pagination;
+import com.pedromonteiro.domain.video.Resource;
 import com.pedromonteiro.infrastructure.api.VideoAPI;
+import com.pedromonteiro.infrastructure.utils.HashingUtils;
 import com.pedromonteiro.infrastructure.video.models.CreateVideoRequest;
 import com.pedromonteiro.infrastructure.video.models.UpdateVideoRequest;
 import com.pedromonteiro.infrastructure.video.models.VideoListResponse;
 import com.pedromonteiro.infrastructure.video.models.VideoResponse;
 
 public class VideoController implements VideoAPI {
+
+    
+    private final CreateVideoUseCase createVideoUseCase;
+    private final GetVideoByIdUseCase getVideoByIdUseCase;
+    private final UpdateVideoUseCase updateVideoUseCase;
+    private final DeleteVideoUseCase deleteVideoUseCase;
+    private final ListVideosUseCase listVideosUseCase;
+    private final GetMediaUseCase getMediaUseCase;
+    private final UploadMediaUseCase uploadMediaUseCase;
+
+    public VideoController(
+            final CreateVideoUseCase createVideoUseCase,
+            final GetVideoByIdUseCase getVideoByIdUseCase,
+            final UpdateVideoUseCase updateVideoUseCase,
+            final DeleteVideoUseCase deleteVideoUseCase,
+            final ListVideosUseCase listVideosUseCase,
+            final GetMediaUseCase getMediaUseCase,
+            final UploadMediaUseCase uploadMediaUseCase
+    ) {
+        this.createVideoUseCase = Objects.requireNonNull(createVideoUseCase);
+        this.getVideoByIdUseCase = Objects.requireNonNull(getVideoByIdUseCase);
+        this.updateVideoUseCase = Objects.requireNonNull(updateVideoUseCase);
+        this.deleteVideoUseCase = Objects.requireNonNull(deleteVideoUseCase);
+        this.listVideosUseCase = Objects.requireNonNull(listVideosUseCase);
+        this.getMediaUseCase = Objects.requireNonNull(getMediaUseCase);
+        this.uploadMediaUseCase = Objects.requireNonNull(uploadMediaUseCase);
+    }
 
     @Override
     public Pagination<VideoListResponse> list(String search, int page, int perPage, String sort, String direction,
@@ -22,12 +61,44 @@ public class VideoController implements VideoAPI {
     }
 
     @Override
-    public ResponseEntity<?> createFull(String title, String description, Integer yearLaunched, Double duration,
-            Boolean opened, Boolean published, String rating, Set<String> categories, Set<String> castMembers,
-            Set<String> genres, MultipartFile videoFile, MultipartFile trailerFile, MultipartFile bannerFile,
-            MultipartFile thumbFile, MultipartFile thumbHalfFile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createFull'");
+    public ResponseEntity<?> createFull(
+            final String aTitle,
+            final String aDescription,
+            final Integer launchedAt,
+            final Double aDuration,
+            final Boolean wasOpened,
+            final Boolean wasPublished,
+            final String aRating,
+            final Set<String> categories,
+            final Set<String> castMembers,
+            final Set<String> genres,
+            final MultipartFile videoFile,
+            final MultipartFile trailerFile,
+            final MultipartFile bannerFile,
+            final MultipartFile thumbFile,
+            final MultipartFile thumbHalfFile
+    ) {
+        final var aCmd = CreateVideoCommand.with(
+                aTitle,
+                aDescription,
+                launchedAt,
+                aDuration,
+                wasOpened,
+                wasPublished,
+                aRating,
+                categories,
+                genres,
+                castMembers,
+                resourceOf(videoFile),
+                resourceOf(trailerFile),
+                resourceOf(bannerFile),
+                resourceOf(thumbFile),
+                resourceOf(thumbHalfFile)
+        );
+
+        final var output = this.createVideoUseCase.execute(aCmd);
+
+        return ResponseEntity.created(URI.create("/videos/" + output.id())).body(output);
     }
 
     @Override
@@ -66,4 +137,20 @@ public class VideoController implements VideoAPI {
         throw new UnsupportedOperationException("Unimplemented method 'uploadMediaByType'");
     }
     
+    private Resource resourceOf(final MultipartFile part) {
+        if (part == null) {
+            return null;
+        }
+
+        try {
+            return Resource.with(
+                    part.getBytes(),
+                    HashingUtils.checksum(part.getBytes()),
+                    part.getContentType(),
+                    part.getOriginalFilename()
+            );
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
 }
